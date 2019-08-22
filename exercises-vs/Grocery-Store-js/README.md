f8b8664e0261234dcef74ed8fcdc58160e751e3e
Randon Shaw <randon.shaw@gmail.com>
2019-08-22 10:29:11 -0600

    exercises

---
+++ exercises-vs/Grocery-Store-js/README.md
@@ -0,0 +1,4 @@
+# Grocery-Store-js
+
+This is the assignment associated with the Grocery Store JS exercise, which can be found at:
+https://coursework.vschool.io/grocery-store-js/
---
+++ exercises-vs/Grocery-Store-js/index.js
@@ -0,0 +1,15 @@
+// https://coursework.vschool.io/grocery-store-js/
+
+const shopper = {
+    firstName: "Randon",
+    lastName: "Shaw",
+    groceryCart: ['bread', 'butter', 'milk', 'cheese', 'cereal'],
+    customerId: 7391,
+    valuedCustomer: function () {
+        if (valuedCustomer === true){
+            return "Welcome ${firstName}. Thank you for being our Valued Customer. Please come again!"
+        }
+    }
+}
+
+console.log(shopper)
